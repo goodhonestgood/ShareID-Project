40e8ef95901160fc68a1a30378e41929e466a9b4
goodhonestgood <tktk517@naver.com>
2022-04-02 23:58:21 +0900

    collection in collection error

--- src/store/modules/Chat.js
+++ src/store/modules/Chat.js
@@ -4,6 +4,16 @@ import {
     setDoc, Timestamp, collection, query, where, getDocs, doc, updateDoc, arrayUnion, arrayRemove, limit, orderBy, collectionGroup
 } from "firebase/firestore"
 
+const getId = async (chatlist, roomId) => {
+    const q = query(chatlist, where("roomId","==", roomId),limit(1))
+    let aid = ""
+    const aDoc = await getDocs(q)
+    aDoc.forEach(doc=>{
+        aid = doc.id
+    })
+    console.log(aid)
+    return aid
+}
 
 const state = {
 
@@ -17,8 +27,9 @@ const actions = {
     // 여기부터
     async addChat(context, { roomId, text }) {
         try {
-            const aid = await getId(roomId)     
-            await setDoc(doc(chatlist, aid, 'messages'), {
+            const chatlist = collection(db, 'chatlist')
+            const aid = await getId(chatlist, roomId)
+            await setDoc(doc(chatlist, "0320test", "messages"), { // error
                 user: context.rootState.user.email,
                 text: text,
                 time: Timestamp.now(),
@@ -33,17 +44,7 @@ const getters = {
 
 }
 
-const getId = async (roomId) => {
-    const chatlist = collection(db, 'chatlist')
-    const q = query(chatlist, where("roomId","==", roomId),limit(1))
-    let aid = ""
-    const aDoc = await getDocs(q)
-    aDoc.forEach(doc=>{
-        aid = doc.id
-    })
-    
-    return aid
-}
+
 
 export default {
     namespaced : true,
