825541c3cfe06c65915f0d1978d27ac0ab95cb0b
goodhonestgood <tktk517@naver.com>
2022-04-06 20:53:41 +0900

    방들어가기 modal 추가 내용 수정중

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,4 @@
 <template>
-<!--state == false인  채팅 방 보기-->
 <div class="container">
     <FilterNav />
     <div v-if="allChatRooms.length > 0" class="row">
@@ -7,13 +6,16 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">{{room.roomName}}</h5>
-                    <p class="card-text">{{room.state}}</p>
-                    <button @click="comein(room.roomId)" class="btn btn-primary">방 들어가기</button>
+                    <p class="card-text">{{room.type}}</p>
+                    <button @click="select(room)" class="btn btn-primary">방 들어가기</button>
                 </div>
             </div>
         </div>
     </div>
     <div v-else> 빈 방이 없습니다 </div>
+    <ConfirmModal @close="toggleModal" :modalActive ="modalActive">
+        {{selectedRoom.users}}
+    </ConfirmModal>
 </div>
 </template>
 
@@ -22,6 +24,7 @@ import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { ref, onMounted, computed } from 'vue'
 import FilterNav from '../components/FilterNav.vue'
+import ConfirmModal from '../components/ConfirmModal.vue'
 
 export default {
     components: {
@@ -35,11 +38,23 @@ export default {
             store.dispatch("ChatRoomModule/intoRoom", { roomId: roomId });
             router.push({ name: "ChatRoom", params: { id: roomId } });
         };
+        
+        const modalActive = ref(false)
+        const toggleModal = () => {
+            modalActive.value = !modalActive.value
+        }
+
+        const selectedRoom = ref({})
+        const select = (room) => {
+            selectedRoom.value = room
+            toggleModal()
+        }
+
         onMounted(() => {
             store.dispatch("ChatRoomModule/getAllRoom", {type: 'All'});
         });
-        return { allChatRooms, comein };
+        return { allChatRooms, comein, toggleModal, modalActive, select, selectedRoom };
     },
-    components: { FilterNav }
+    components: { FilterNav, ConfirmModal }
 }
 </script>
