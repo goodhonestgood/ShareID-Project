3082bd5c278918b87031198edaa8bf849ec8012e
goodhonestgood <tktk517@naver.com>
2022-04-13 23:43:48 +0900

    가능한 타입만 보여주기 수정

--- src/views/ChatList.vue
+++ src/views/ChatList.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="chatlist">
-        <p># 방 만들기</p>
-        <form @submit.prevent="handleMake">
-            <input class="form-control me-2 mb-2 text-secondary" type="text" placeholder="방 이름 입력" aria-label="input" v-model="roomName">
-            <div v-for="type in types">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" :value="type" v-model="selectedType">
-                    <label class="form-check-label" for="inlineRadio1">{{type}}</label>
+        <div v-if ="types.length">
+            <p># 방 만들기</p>
+            <form @submit.prevent="handleMake">
+                <input class="form-control me-2 mb-2 text-secondary" type="text" placeholder="방 이름 입력" aria-label="input" v-model="roomName">
+                <div v-for="type in types">
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" :value="type" v-model="selectedType">
+                        <label class="form-check-label" for="inlineRadio1">{{type}}</label>
+                    </div>
                 </div>
-            </div>
 
-            <button class="btn btn-outline-secondary" type="submit"> OK </button>
-        </form>
-        <hr />
+                <button class="btn btn-outline-secondary" type="submit"> OK </button>
+            </form>
+            <hr />
+        </div>
         <p># 내 채팅 목록</p>
         <div v-if="chatLists" class="list-group">
             <div class="mb-2" v-for="list in chatLists">
@@ -38,11 +40,12 @@ export default {
         const types = computed(()=>{
             let result = []
             let dict = store.state.ChatRoomModule.roomOfUser
-            for(val of dict) {
+            for(let val in dict) {
                 if (dict[val] == false) {
                     result.push(val)
                 } 
             }
+            console.log(result)
             return result
         })
         const selectedType = ref("")
